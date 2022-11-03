dd30fe0cc5c04861adaea64823a5bd311f10d76c
Eder Jose <e.pariona@costamar.com>
2022-11-02 19:48:48 -0500

    initial commit

---
+++ src/main/java/com/ederrafo/soapservicebottomup/SoapServiceBottomUpApplication.java
@@ -0,0 +1,13 @@
+package com.ederrafo.soapservicebottomup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceBottomUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapServiceBottomUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ederrafo/soapservicebottomup/SoapServiceBottomUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederrafo.soapservicebottomup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceBottomUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
